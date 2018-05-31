936414e934ede140a8aee2c859f8562d0f3c1dc7
Charlie Meyer <meyercharl@eisti.eu>
2018-05-31 08:56:16 +0200

    Ajout du fichier test de l'affichage

--- Test.pas
+++ Test.pas
@@ -7,16 +7,17 @@ Var
 Begin
     Randomize();
     jeux := CountParam();
+    jeux.player[1].nom := 'Charlie';
     jeux.player[1].main[0].couleur := 1;
     jeux.player[1].main[0].forme := 1;
     jeux.player[1].main[1].couleur := 3;
-    jeux.player[1].main[0].forme := 3;
+    jeux.player[1].main[1].forme := 3;
     jeux.player[1].main[2].couleur := 5;
-    jeux.player[1].main[0].forme := 5;
+    jeux.player[1].main[2].forme := 5;
     jeux.player[1].main[3].couleur := 7;
-    jeux.player[1].main[0].forme := 7;
+    jeux.player[1].main[3].forme := 7;
     jeux.player[1].main[4].couleur := 2;
-    jeux.player[1].main[0].forme := 2;
+    jeux.player[1].main[4].forme := 2;
     jeux.player[1].main[5].couleur := 8;
     jeux.player[1].main[5].forme := 8;
     For i:=0 to 24 do
