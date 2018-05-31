c9d2fe3d874498509bf0935634a846550fa87e00
Guillaume Proton <guillaume.proton@eisti.fr>
2018-05-31 10:19:33 +0200

    Création fonction VerifPieceNord

---
+++ Fonctions Guillaume/FUNCTION VerifPieceNord.pas
@@ -0,0 +1,26 @@
+(*
+--------------------------------------------------------
+- Fonction         : VerifPieceNord
+- Auteur           : Guillaume Proton
+- Date de creation : 31 Mai 2018
+-
+- But              : Renvoie 0 si la pièce voulant être jouée n'est pas de la même couleur ou de la même forme que la couleur ou la forme de la pièce située au nord sinon 1
+- Remarques        : Aucune
+- Pré conditions   : Aucune
+- Post conditions  : Renvoie 0 si la pièce voulant être jouée n'est pas de la même couleur ou de la même forme que la couleur ou la forme de la pièce située au nord sinon 1
+--------------------------------------------------------*)
+
+
+Function VerifPieceNord(jeux:jeu ; couleur_p,forme_p,i,j : integer):integer;
+    memeParam : integer;
+Begin
+    if ((couleur_p = jeux.grille[i-1,j].couleur) xor (forme_p = jeux.grille[i-1,j].forme)) then    // "xor" car on ne peut pas jouer une pièce de la même forme et de la même couleur côte à côte
+    Begin
+        memeParam:=1
+    End
+    else
+    begin
+        memeParam:=0;
+    end;
+    VerifPieceNord:=memeParam;
+End;
