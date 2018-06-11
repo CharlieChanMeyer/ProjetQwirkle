2e6904c10fc470f187a857d2746a07f9d98e7225
Guillaume Proton <guillaume.proton@eisti.fr>
2018-06-11 11:38:34 +0200

    UnitLegalite prete

--- UnitLegalite.pas
+++ UnitLegalite.pas
@@ -18,19 +18,58 @@ interface
 USES SysUtils,UnitType,UnitParam,UnitAff,Crt;
 
 Function PrioriteJoueur(jeux:jeu):jeu;
-Function CaseVide(jeux:jeu ; i,j:integer):boolean;
-Function LegaliteCoup1(jeux:jeu; couleur_p,forme_p,i,j :integer):integer;
+Function deuxMemesAttributs(jeux:jeu;i,j:integer):integer;
+Function CaseVide(jeux:jeu ; i,j:integer):integer;
+Function LegaliteCoup(jeux:jeu; couleur_p,forme_p,i,j :integer):integer;
 Function VerifPieceEst(jeux:jeu ; couleur_p,forme_p,i,j : integer):integer;
 Function VerifPieceNord(jeux:jeu ; couleur_p,forme_p,i,j : integer):integer;
 Function VerifPieceOuest(jeux:jeu ; couleur_p,forme_p,i,j : integer):integer;
 Function VerifPieceSud(jeux:jeu ; couleur_p,forme_p,i,j : integer):integer;
-
+Function Max(x1,x2:integer):integer;
+Function nbPiecesNord(jeux:jeu;i,j:integer):integer;
+Function nbPiecesSud(jeux:jeu;i,j:integer):integer;
+Function nbPiecesOuest(jeux:jeu;i,j:integer):integer;
+Function nbPiecesEst(jeux:jeu;i,j:integer):integer;
 
 implementation
 
 jeux.player.main[].forme
 
 
+(*
+--------------------------------------------------------
+- Fonction         : deuxMemesAttributs
+- Auteur           : Guillaume Proton
+- Date de creation : 28 Mai 2018
+-
+- But              : Renvoie 1 s'il y a deux fois le même attribut sur la même ligne sinon 0
+- Remarques        : Aucune  
+- Pré conditions   : Aucune
+- Post conditions  : Renvoie 1 s'il y a deux fois le même attribut sur la même ligne sinon 0
+--------------------------------------------------------*)
+
+Function deuxMemesAttributs(jeux:jeu;i,j : integer):integer;
+Var
+    l, memeAttribut:integer;
+
+Begin
+    memeAttribut :=0;
+    for l:=i+nbCasesSud(jeux,i,j) downto i-nbCasesNord(jeux,i,j) do             // parcoure la ligne contenant la pièce à la position (i,j) de haut en bas
+    Begin
+        if (((jeux.grille[i,j].forme=jeux.grille[l,j].forme) or (jeux.grille[i,j].couleur=jeux.grille[l,j].couleur)) and (l<>i)) then  // s'il y a une pièce ayant le même attribut sur cette ligne autre que la pièce de la position (i,j)
+        Begin
+           memeAttribut := 1
+        End;
+    End;
+    for l:=j+nbCasesEst(jeux,i,j) downto j-nbcasesOuest(jeux,i,j) do            // parcoure la ligne contenant la pièce à la position (i,j) de gauche à droite
+    Begin
+        if (((jeux.grille[i,j].forme=jeux.grille[i,l].forme) or (jeux.grille[i,j].couleur=jeux.grille[i,l].couleur)) and (l<>j)) then // s'il y a une pièce ayant le même attribut sur cette ligne autre que la pièce de la position (i,j)
+        Begin
+            memeAttribut:=1
+        end;
+    end;
+    deuxMemesAttributs:=memeAttribut ;
+End;
 
 
 (*
@@ -39,21 +78,31 @@ jeux.player.main[].forme
 - Auteur : Guillaume Proton
 - Date de creation : 28 Mai 2018
 -
-- But : Renvoie TRUE si il n'y a pas de pièce dans cette case sinon renvoie FALSE
+- But : Renvoie 0 s'il n'y a pas de pièce dans cette case sinon renvoie 1
 - Remarques : Aucune
 - Pré conditions : Aucune
-- Post conditions : Renvoie TRUE si il n'y a pas de pièce dans cette case sinon renvoie FALSE
+- Post conditions : Renvoie 0 s'il n'y a pas de pièce dans cette case sinon renvoie 1
 --------------------------------------------------------*)
 
-Function CaseVide(jeux:jeu ; i,j:integer):boolean;
+Function CaseVide(jeux:jeu ; i,j:integer):integer;
+Var
+    Vide : integer;
 Begin
-    CaseVide:=((jeux.grille[i,j].couleur=0) and (jeux.grille[i,j].forme=0));
+    if ((jeux.grille[i,j].couleur=0) and (jeux.grille[i,j].forme=0)) then   // si la couleur et la forme de la pièce censée être à cette position ne sont pas définies (=0) alors il n'y a pas de pièce
+    Begin
+        Vide:=0;
+    End
+    Else
+    Begin
+        Vide:=1
+    End;
+    CaseVide:=Vide;
 End;
 
 
 (*
 --------------------------------------------------------
-- Fonction         : LegaliteCoup1
+- Fonction         : LegaliteCoup
 - Auteur           : Guillaume Proton
 - Date de creation : 28 Mai 2018
 -
@@ -63,7 +112,7 @@ End;
 - Post conditions  : Vérifie la légalité du coup à la position (i,j)(0 : coup impossible, 1 : coup accepté)
 --------------------------------------------------------*)
 
-Function LegaliteCoup1(jeux:jeu; couleur_p,forme_p,i,j :integer):integer;
+Function LegaliteCoup(jeux:jeu; couleur_p,forme_p,i,j :integer):integer;
 Var
     CoupLegal : integer;
 Begin
@@ -85,7 +134,11 @@ Begin
     Begin
         CoupLegal:=0;               // coup impossible car il y a déjà une pièce à cette position
     End;
-    LegaliteCoup1:=CoupLegal;
+    if (deuxMemesAttributs(jeux,i,j)=1) then  // s'il y a deux fois le même attribut sur la même ligne alors le coup n'est pas valide
+    Begin
+        CoupLegal:=0
+    end;
+    LegaliteCoup:=CoupLegal;
 End;
 
 
@@ -204,6 +257,141 @@ Begin
     VerifPieceSud:=memeParam;
 End;
 
+
+(*
+--------------------------------------------------------
+- Fonction         : Max
+- Auteur           : Guillaume Proton
+- Date de creation : 11 Juin 2018
+-
+- But              : Renvoie le maximum entre deux entiers 
+- Remarques        : Aucune  
+- Pré conditions   : Aucune
+- Post conditions  : Renvoie le maximum entre deux entiers
+--------------------------------------------------------*)
+
+Function Max(x1,x2 : integer):integer;
+Var
+   maximum : integer;
+Begin
+   if (x1>x2) then
+   Begin
+      maximum:=x1;
+   end
+   else
+   Begin
+      maximum:=x2
+   End;
+   Max:=maximum;
+End;
+
+(*
+--------------------------------------------------------
+- Fonction         : nbPiecesNord
+- Auteur           : Guillaume Proton
+- Date de creation : 7 Juin 2018
+-
+- But              : Renvoie le nombre de pièces "collées" entre elles au nord de la pièce située à la position (i,j)
+- Remarques        : Aucune  
+- Pré conditions   : Aucune
+- Post conditions  : Renvoie le nombre de pièces "collées" entre elles au nord de la pièce située à la position (i,j)
+--------------------------------------------------------*)
+
+Function nbPiecesNord(jeux:jeu;i,j:integer):integer;
+Var
+    l,ComptePiece : integer;
+Begin
+    ComptePiece:=0;
+    for l:=1 to Max(jeux.parametre.nbforme,jeux.parametre.nbcouleur)-1  do         //Cet itérateur va jusqu'au maximum entre le nombre de couleurs différentes et le nombre de formes différentes présentes dans le jeu et définies dans les paramètres moins 1 car une ligne ne peut dépasser ce nombre maximum
+    Begin
+        if (CaseVide(jeux,i-l,j)=1) then      // s'il y a une pièce dans la case située au-dessus alors ...
+        Begin
+            ComptePiece := ComptePiece +1         // ... on incrémente à chaque fois que la case contient une pièce
+        End
+    End;
+    nbPiecesNord:=ComptePiece;
+End;
+
+(*
+--------------------------------------------------------
+- Fonction         : nbPiecesSud
+- Auteur           : Guillaume Proton
+- Date de creation : 7 Juin 2018
+-
+- But              : Renvoie le nombre de pièces "collées" entre elles au sud de la pièce située à la position (i,j)
+- Remarques        : Aucune  
+- Pré conditions   : Aucune
+- Post conditions  : Renvoie le nombre de pièces "collées" entre elles au sud de la pièce située à la position (i,j)
+--------------------------------------------------------*)
+
+Function nbPiecesSud(jeux:jeu;i,j:integer):integer;
+Var
+    l,ComptePiece : integer;
+Begin
+    ComptePiece:=0;
+    for l:=1 to Max(jeux.parametre.nbforme,jeux.parametre.nbcouleur)-1  do         //Cet itérateur va jusqu'au maximum entre le nombre de couleurs différentes et le nombre de formes différentes présentes dans le jeu et définies dans les paramètres moins 1 car une ligne ne peut dépasser ce nombre maximum
+    Begin
+        if (CaseVide(jeux,i+l,j)=1) then      // s'il y a une pièce dans la case située au-dessus alors ...
+        Begin
+            ComptePiece := ComptePiece +1         // ... on incrémente à chaque fois que la case contient une pièce
+        End
+    End;
+    nbPiecesSud:=ComptePiece;
+End;
+
+(*
+--------------------------------------------------------
+- Fonction         : nbPiecesOuest
+- Auteur           : Guillaume Proton
+- Date de creation : 7 Juin 2018
+-
+- But              : Renvoie le nombre de pièces "collées" entre elles à l'ouest de la pièce située à la position (i,j)
+- Remarques        : Aucune  
+- Pré conditions   : Aucune
+- Post conditions  : Renvoie le nombre de pièces "collées" entre elles à l'ouest de la pièce située à la position (i,j)
+--------------------------------------------------------*)
+
+Function nbPiecesOuest(jeux:jeu;i,j:integer):integer;
+Var
+    l,ComptePiece : integer;
+Begin
+    ComptePiece:=0;
+    for l:=1 to Max(jeux.parametre.nbforme,jeux.parametre.nbcouleur)-1  do         //Cet itérateur va jusqu'au maximum entre le nombre de couleurs différentes et le nombre de formes différentes présentes dans le jeu et définies dans les paramètres moins 1 car une ligne ne peut dépasser ce nombre maximum
+    Begin
+        if (CaseVide(jeux,i,j-l)=1) then      // s'il y a une pièce dans la case située au-dessus alors ...
+        Begin
+            ComptePiece := ComptePiece +1         // ... on incrémente à chaque fois que la case contient une pièce
+        End
+    End;
+    nbPiecesOuest:=ComptePiece;
+End;
+
+(*
+--------------------------------------------------------
+- Fonction         : nbPiecesEst
+- Auteur           : Guillaume Proton
+- Date de creation : 7 Juin 2018
+-
+- But              : Renvoie le nombre de pièces "collées" entre elles à l'est de la pièce située à la position (i,j)
+- Remarques        : Aucune  
+- Pré conditions   : Aucune
+- Post conditions  : Renvoie le nombre de pièces "collées" entre elles à l'est de la pièce située à la position (i,j)
+--------------------------------------------------------*)
+
+Function nbPiecesEst(jeux:jeu;i,j:integer):integer;
+Var
+    l,ComptePiece : integer;
+Begin
+    ComptePiece:=0;
+    for l:=1 to Max(jeux.parametre.nbforme,jeux.parametre.nbcouleur)-1  do         //Cet itérateur va jusqu'au maximum entre le nombre de couleurs différentes et le nombre de formes différentes présentes dans le jeu et définies dans les paramètres moins 1 car une ligne ne peut dépasser ce nombre maximum
+    Begin
+        if (CaseVide(jeux,i,j+l)=1) then      // s'il y a une pièce dans la case située au-dessus alors ...
+        Begin
+            ComptePiece := ComptePiece +1         // ... on incrémente à chaque fois que la case contient une pièce
+        End
+    End;
+    nbPiecesEst:=ComptePiece;
+End;
 
 END.
 
