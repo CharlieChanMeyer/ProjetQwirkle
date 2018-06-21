5a63eb7e5222a6db2bb27a63989cb8473cd4095b
Charlie Meyer <meyercharl@eisti.eu>
2018-06-21 12:45:39 +0200

    Commit de qwirkle

---
+++ qwirkle.pas
@@ -0,0 +1,26 @@
+(*
+--------------------------------------------------------
+- Fichier : Qwirkle.pas
+- Auteur : Charlie MEYER / Guillaume PROTON / Marco ESPIOT
+- Date de creation : Mardi 19 Juin 2018
+-
+- But : Jouer au jeu de societe Qwirkle
+- Remarques : Aucune
+- Compilation : fpc
+- Edition des liens : fpc
+- Execution : shell
+--------------------------------------------------------
+*)
+
+PROGRAM qwirkle;
+uses SysUtils,UnitType,UnitParam,UnitAff,UnitTour,UnitMarco,UnitLegalite,Crt;
+Var
+    jeux : jeu;
+Begin
+    Randomize();
+    jeux := CountParam();
+    jeux := initMain(jeux);
+    jeux := initJoueur(jeux);
+    jeux.player:= prioriteJoueur(jeux);
+    Tourdejeu(jeux)
+End.
